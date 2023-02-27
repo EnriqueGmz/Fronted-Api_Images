8d3710ea0b7b0a8c830ea723696450250a58a1ea
EnriqueGmz <enriquegomezs@gmail.com>
2023-02-27 23:24:13 +0100

    image-form, upload images hecho

--- src/app/components/image-form/image-form.component.ts
+++ src/app/components/image-form/image-form.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ImagesService } from 'src/app/services/images.service';
 
 @Component({
   selector: 'app-image-form',
   templateUrl: './image-form.component.html',
-  styleUrls: ['./image-form.component.css']
+  styleUrls: ['./image-form.component.css'],
 })
 export class ImageFormComponent implements OnInit {
+  image = '';
+  imgURL = 'assets/no-image.png';
 
-  constructor() { }
+  constructor(private imageService: ImagesService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectImage(event: any): void {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.imgURL = event.target.result;
+      };
+      this.image = file;
+      reader.readAsDataURL(file);
+      console.log(this.image);
+    }
   }
 
+  uploadImage(
+    title: HTMLInputElement,
+    descriptionImage: HTMLTextAreaElement
+  ): boolean {
+    this.imageService
+      .createImage(title.value, descriptionImage.value, this.image)
+      .subscribe(
+        (res) => console.log(res),
+        (err) => console.log(err)
+      );
+    return false;
+  }
 }
--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ImagesService {
+  private URI: string = 'http://localhost:4000/api/v1/images';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createImage(title: string, descriptionImage: string, image: string) {
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('descriptionImage', descriptionImage);
+    formData.append('image', image);
+
+    return this.http.post(this.URI, formData);
+  }
 }
